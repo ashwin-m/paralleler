d92c682e8df65791286a37d6f2e7d40763824bf1
Ashwin Mathew <ashwin12028@iiitd.ac.in>
2025-02-01 12:11:48 +0800

    [NEW] add new basic submit api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket::response::status;
+use rocket::form::Form;
+use rocket::fs::TempFile;
+
+#[derive(FromForm)]
+struct Upload<'r> {
+    file: TempFile<'r>,
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[post("/submit", data = "<upload>")]
+fn submit(upload: Form<Upload<'_>>) -> status::Accepted<&'static str> {
+    status::Accepted("test")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, submit])
 }
